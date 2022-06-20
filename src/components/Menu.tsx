761790a95b982e3930bee4674165698106a26ff7
Klaus Nielsen <klnie1989@gmail.com>
2022-06-20 14:44:48 +0200

    Finished menu styles Added fade transition and fixed styles

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useTrail, animated } from 'react-spring';
+import { useTransition, useTrail, animated } from 'react-spring';
 import type { IMenu } from 'interfaces/menu';
 
+const Backdrop = styled.div`
+  position: absolute;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+`;
+
+const AnimatedBackdrop = animated(Backdrop);
+
 const Container = styled.div`
-  position: relative;
+  position: absolute;
+  z-index: 2;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
   height: 100%;
-  overflow: hidden;
-  cursor: pointer;
 `;
 
 const Item = styled.div`
-  position: relative;
   width: 100%;
   height: 80px;
   line-height: 80px;
@@ -23,8 +31,6 @@ const Item = styled.div`
   font-size: 5em;
   font-weight: 800;
   text-transform: uppercase;
-  will-change: transform, opacity;
-  overflow: hidden;
 `;
 
 const AnimatedItem = animated(Item);
@@ -32,6 +38,7 @@ const AnimatedItem = animated(Item);
 const Text = styled.div`
   overflow: hidden;
   user-select: none;
+  cursor: pointer;
 `;
 
 const AnimatedText = animated(Text);
@@ -41,35 +48,51 @@ export default function Menu(props: IMenu) {
 
   const config = { mass: 5, tension: 2000, friction: 200 };
 
+  const fade = useTransition(open, {
+    config,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
+  });
+
   const trail = useTrail(items.length, {
     config,
     opacity: open ? 1 : 0,
     x: open ? 0 : 20,
     y: open ? 20 : 0,
     height: open ? 80 : 0
-    // from: { opacity: 0, x: 20, y: 0, height: 0 }
   });
 
   return (
-    <Container>
-      <nav>
-        {trail.map(({ x, y, height, ...rest }, index) => {
-          const { name, action } = items[index];
-          return (
-            <AnimatedItem
-              key={name}
-              style={{
-                ...rest,
-                transform: y.interpolate((y) => `translate3d(${y}px, 0, 0)`)
-              }}
-            >
-              <AnimatedText style={{ height }} onClick={() => changePage(action)}>
-                {name}
-              </AnimatedText>
-            </AnimatedItem>
-          );
-        })}
-      </nav>
-    </Container>
+    <>
+      {fade(
+        ({ opacity }, item) =>
+          item && (
+            <>
+              <AnimatedBackdrop style={{ opacity }} />
+              <Container>
+                <nav>
+                  {trail.map(({ x, y, height, ...rest }, index) => {
+                    const { name, action } = items[index];
+                    return (
+                      <AnimatedItem
+                        key={name}
+                        style={{
+                          ...rest,
+                          transform: y.interpolate((y) => `translate3d(${y}px, 0, 0)`)
+                        }}
+                      >
+                        <AnimatedText style={{ height }} onClick={() => changePage(action)}>
+                          {name}
+                        </AnimatedText>
+                      </AnimatedItem>
+                    );
+                  })}
+                </nav>
+              </Container>
+            </>
+          )
+      )}
+    </>
   );
 }
